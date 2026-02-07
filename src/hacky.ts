2ebcd147a58748cd718c9340cfb504338ea644c0
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2026-02-07 14:47:57 +0000

    package.json

--- src/hacky.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-// This is a hack!
-const hacky = (): string => {
-    /* This hack does not count */
-    let hack: number = 3;
-    let another: number = 9;
-    return 'hacky string';
-}
