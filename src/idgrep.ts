a2d80a94600f2cbac806ad94251f98650e2632c3
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2026-02-07 04:43:06 +0000

    first ts version of idgrep

--- src/idgrep.ts
+++ src/idgrep.ts
@@ -4,37 +4,51 @@ import * as fs from "fs";
 import * as esprima from "espree";
 import { Command } from "commander";
 import * as packageJson from "../package.json";
-import * as estraverse from "estraverse";
+import { traverse } from "estraverse";
 
-interface Location {
-  line: number;
-  column: number;
-}
-
+// Define a minimal compatible Node interface that matches what we expect from the AST
 interface ASTNode {
   type: string;
   name?: string;
   loc?: {
-    start: Location;
-    end: Location;
-  };
-  range?: [number, number];
+    start: {
+      line: number;
+      column: number;
+    };
+    end: {
+      line: number;
+      column: number;
+    };
+  } | null;
+}
+
+// Type guard to check if a node is an Identifier with proper typing
+function isIdentifierWithName(node: ASTNode): node is ASTNode & { 
+  type: "Identifier"; 
+  name: string; 
+  loc: NonNullable<ASTNode["loc"]> 
+} {
+  return node.type === "Identifier" && 
+         typeof node.name === "string" && 
+         node.loc !== null && 
+         node.loc !== undefined;
 }
 
 const idgrep = function (pattern: RegExp, code: string, filename: string): void {
   const lines: string[] = code.split("\n");
   if (/^#!/.test(lines[0])) code = code.replace(/^.*/, ""); // Avoid line "#!/usr/bin/env node"
   
+  // Parse AST with proper espree options
   const ast = esprima.parse(code, {
     ecmaVersion: 6,
     loc: true,
     range: true,
-  }) as any;
+  });
   
-  estraverse.traverse(ast, {
-    enter: function (node: any, parent?: any): void {
-      if (node.type === "Identifier" && node.name && pattern.test(node.name)) {
-        if (!node.loc) return;
+  // Traverse with type-safe handling
+  traverse(ast as any, {
+    enter: function (node: ASTNode, parent: ASTNode | null): void {
+      if (isIdentifierWithName(node) && pattern.test(node.name)) {
         const loc = node.loc.start;
         const line = loc.line - 1;
         console.log(
